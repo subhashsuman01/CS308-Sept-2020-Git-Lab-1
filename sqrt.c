6eae3d3dbe21d33ba1c6c37943a6a46ad8c85438
subhashsuman01 <subhashsuman01@gmail.com>
2020-10-16 16:40:50 +0530

    added prefix debug

--- sqrt.c
+++ sqrt.c
@@ -11,20 +11,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Usage: sqrt input\n\n");
+		printf("DEBUG: Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
 	if (input < 0)
 	{
-		printf("Sqrt of %d is %fi\n", input, sqrt(abs(input)));
+		printf("DEBUG: Sqrt of %d is %fi\n", input, sqrt(abs(input)));
 	}
 	else
 	{
-		printf("Sqrt of %d is %f\n", input, sqrt(input));
+		printf("DEBUG: Sqrt of %d is %f\n", input, sqrt(input));
 	}
-	printf("End of program. Exiting.");
+	printf("DEBUG: End of program. Exiting.");
 	return (0);
 
 } // end main
