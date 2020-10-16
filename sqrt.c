a939707ee7b2cad80d8825bd8eb37c43d97980a8
subhashsuman01 <subhashsuman01@gmail.com>
2020-10-16 16:39:24 +0530

    added prefix neggsupport

--- sqrt.c
+++ sqrt.c
@@ -11,20 +11,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Usage: sqrt input\n\n");
+		printf("NEGSUPPORT: Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
 	if (input < 0)
 	{
-		printf("Sqrt of %d is %fi\n", input, sqrt(abs(input)));
+		printf("NEGSUPPORT: Sqrt of %d is %fi\n", input, sqrt(abs(input)));
 	}
 	else
 	{
-		printf("Sqrt of %d is %f\n", input, sqrt(input));
+		printf("NEGSUPPORT: Sqrt of %d is %f\n", input, sqrt(input));
 	}
-	printf("End of program. Exiting.");
+	printf("NEGSUPPORT: End of program. Exiting.");
 	return (0);
 
 } // end main
