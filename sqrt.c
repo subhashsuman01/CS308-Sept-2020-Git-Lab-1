7377a311b4ecc1e640c87bb1adc352c5f441d1b1
subhashsuman01 <subhashsuman01@gmail.com>
2020-10-16 16:32:59 +0530

    deleted print statement

--- sqrt.c
+++ sqrt.c
@@ -24,7 +24,6 @@ int main(int argc, char *argv[])
 	{
 		printf("Sqrt of %d is %f\n", input, sqrt(input));
 	}
-	printf("Currently in br_negativeSupport.");
 	printf("End of program. Exiting.");
 	return (0);
 
